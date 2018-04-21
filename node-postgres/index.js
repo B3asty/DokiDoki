2197f096ac93db31ce189c5aaed0d3a87ff693da
Akashic Bearer <35845153+AkashicBearer@users.noreply.github.com>
2018-04-21 23:51:57 +0200

    Update index.js

--- node-postgres/index.js
+++ node-postgres/index.js
@@ -1,6 +1,6 @@
 const { Pool } = require('pg');
 const pool = new Pool({
-  connectionString: "postgres://mncmdnttojdsku:3849194dc1c60408b302cd9991f3ce27e08827998d5029ae25080e6bf09ee779@ec2-54-225-96-191.compute-1.amazonaws.com:5432/d3u98tvto5gblt",
+  connectionString: process.env.DATABASE_URL,
   ssl: true
 });
 
