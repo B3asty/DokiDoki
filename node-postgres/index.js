afa09599b7ad013d162e4a6e2c2fc3fb7f124782
Akashic Bearer <35845153+AkashicBearer@users.noreply.github.com>
2018-04-21 23:43:38 +0200

    Create index.js

---
+++ node-postgres/index.js
@@ -0,0 +1,17 @@
+const { Pool } = require('pg');
+const pool = new Pool({
+  connectionString: "postgres://mncmdnttojdsku:3849194dc1c60408b302cd9991f3ce27e08827998d5029ae25080e6bf09ee779@ec2-54-225-96-191.compute-1.amazonaws.com:5432/d3u98tvto5gblt",
+  ssl: true
+});
+
+app.get('/db', async (req, res) => {
+  try {
+    const client = await pool.connect()
+    const result = await client.query('SELECT * FROM test_table');
+    res.render('pages/db', result);
+    client.release();
+  } catch (err) {
+    console.error(err);
+    res.send("Error " + err);
+  }
+});
