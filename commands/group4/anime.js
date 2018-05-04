35c277bf30c86c3bb4acbe972206c84f690bef97
Kandrina <manuela.greifoner@gmx.at>
2018-05-04 02:22:59 +0200

    anime

--- commands/group4/anime.js
+++ commands/group4/anime.js
@@ -112,7 +112,7 @@ module.exports = class animeCommand extends Command {
 		  		titles = titles+"\n**Please enter the number of the Anime you want to view** \n**Or type** `cancel` **to cancel the command**"
 		  		embed.setDescription(titles)
 
-				inputAn(result.anime)
+				msg.channel.send(embed)
 
 		  	}else {
 		  		var res = result.anime[0];
