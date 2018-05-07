df56be78ff4ce9d9c5ac1aab3d11711043072e3e
Kandrina <manuela.greifoner@gmx.at>
2018-05-07 17:19:21 +0200

    urban

---
+++ commands/group4/urban.js
@@ -0,0 +1,43 @@
+const { Command } = require('discord.js-commando')
+const { RichEmbed } = require('discord.js');
+
+module.exports = class UrbanCommand extends Command {
+    constructor(client) {
+        super(client, {
+            name: 'urban',
+            aliases: ['ub'],
+            group: 'group4',
+            memberName: 'urban',
+            description: 'Search Something on Urban',
+            examples: ['{prifix}urban [text]'],
+            guildOnly: true,
+            args:[
+                {
+                    key: 'text',
+                    prompt: 'what to search for',
+                    type: 'string',
+                    default: ''
+                }
+            ]
+        });
+    }     
+    async run(msg,args) {
+        const urban = require('relevant-urban','urban');
+        if (!args.text) msg.channel.send(`***Please specify some text!***`);
+        let res = await urban(args.text).catch(e => { 
+          return msg.channel.send('***Sorry, that word was not found!***');
+        });
+        const embed = new RichEmbed()
+          embed.setColor('RANDOM') 
+          embed.setTitle(res.urbanURL) 
+          embed.setDescription(`**Definition of ${res.word}:**\n${res.definition}`)
+          embed.addField('**Examples:**',` ${res.example}`)
+          embed.addField('Author', res.author, true) 
+          embed.addField('Rating', `**\:thumbsup: \`Upvotes: ${res.thumbsUp}\` | :thumbsdown: \`Downvotes: ${res.thumbsDown}\`**`)
+        if (res.tags.length > 0 && res.tags.join(' ').length < 1024) {
+          embed.addField('Tags', res.tags.join(', '), true)
+        }
+        return msg.channel.send(embed);
+      
+      }
+};
