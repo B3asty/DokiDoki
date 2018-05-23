0a4f7c795d2261f2ec0de44adfb79592706d3ab0
Akashic Bearer <35845153+AkashicBearer@users.noreply.github.com>
2018-05-23 18:36:19 +0200

    Update index.js

--- index.js
+++ index.js
@@ -57,10 +57,8 @@ const pool = new Pool({
 });
 client.on("message", message => {
 pool.connect();
-const query = pool.query(
-  'SELECT * FROM XP WHERE userid ="${message.author.id}"').then(row => {
-    if (!row) {
-      pool.query("INSERT INTO XP (userid, xp, level) VALUES (?, ?, ?)", [message.author.id, 1, 0]);
+const query = pool.query(`SELECT * FROM XP WHERE userid ="${message.author.id}"`).then(row => {
+    if (!row) { pool.query("INSERT INTO XP (userid, xp, level) VALUES (?, ?, ?)", [message.author.id, 1, 0]);
     } else {
       let curLevel = Math.floor(0.01 * Math.sqrt(row.points + 0.1));
       if (curLevel > row.level) {
