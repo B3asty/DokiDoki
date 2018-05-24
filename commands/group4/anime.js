2577e3f10fc7b7cf0bb29a379a39875043424b79
Kandrina <manuela.greifoner@gmx.at>
2018-05-24 20:56:16 +0200

    a

--- commands/group4/anime.js
+++ commands/group4/anime.js
@@ -42,7 +42,7 @@ module.exports = class animeCommand extends Command {
 
 
 		const mal = new MALjs('DokiDokiBot', process.env.mal);
-		console.log(mal.MALjs)
+		console.log(mal.anime)
 		const months = {
 				"00": "",
 	            "01": "January ",
