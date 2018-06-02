659ba5d3e33eeb48accdb7427dfccbd92dc4b070
Akashic Bearer <35845153+AkashicBearer@users.noreply.github.com>
2018-06-02 03:13:43 +0200

    Update index.js

--- node-postgres/index.js
+++ node-postgres/index.js
@@ -1,16 +1,28 @@
-const { Client } = require('pg');
-
+const { Client } = require('pg',"discord.js","discord.js-commando");
 const client = new Client({
   connectionString: process.env.DATABASE_URL,
   ssl: true,
 });
-
 client.connect();
-
-client.query('SELECT table_schema,table_name FROM information_schema.tables;', (err, res) => {
+client.query('SELECT * FROM XP', (err, res) => {
   if (err) throw err;
   for (let row of res.rows) {
     console.log(JSON.stringify(row));
   }
   client.end();
 });
+
+-client.on("message", message => {
+-pool.connect();
+-const  query = pool.query(`SELECT * FROM XP WHERE userid = ${message.author.id}`).then(row => {
+-    if (!row) { pool.query("INSERT INTO XP (userid, xp, level) VALUES (?, ?, ?)", [message.author.id, 0, 0]);
+-    } else {
+-      let curLevel = Math.floor(0.01 * Math.sqrt(11 + 0.01));
+-      if (curLevel > row.level) {		
+-        row.level = curLevel;
+-        pool.query(`UPDATE XP SET level = ${row.level} WHERE userid = ${message.author.id}`);
+-        message.reply(`You've leveled up to level **${curLevel}**!`);
+-      }
+-  };
+-})
+-});
