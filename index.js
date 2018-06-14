a0e00e16d74685c43ceeef85b2eddd8143a197fd
Kandrina <manuela.greifoner@gmx.at>
2018-06-14 15:36:49 +0200

    i

--- index.js
+++ index.js
@@ -81,10 +81,9 @@ client.on("message", (message) => {
       console.log(row)
       if(!row){
         pool.query('INSERT INTO XP (userid, xp, level) VALUES ('+message.author.id+', 1, 0,)');
-        var res = pool.query('select xp from xp where userid ='+message.author.id+'').then(res => console.log(res))
+        var res = pool.query('select * from XP where userid ='+message.author.id+'').then(res => console.log(res.xp))
       }else{
-        var res = pool.query('select xp from xp where userid ='+message.author.id+'').then(res => console.log(res))
-        console.log(row.xp)
+        var res = pool.query('select * from XP where userid ='+message.author.id+'').then(res => console.log(res.xp))
         pool.query('update XP set xp='+(row.xp+newxp)+' where userid ='+message.author.id+'')
         console.log(row.xp)
         let curlevel = Math.floor(0.1 * Math.sqrt(row.xp + 0.1));
