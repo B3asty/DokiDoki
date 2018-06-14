bb9f8e639bee9d4d154aa0bd1110d4a5db22960e
Akashic Bearer <35845153+AkashicBearer@users.noreply.github.com>
2018-06-14 18:47:01 +0200

    Update index.js

--- index.js
+++ index.js
@@ -78,21 +78,21 @@ client.on("message", (message) => {
       if(err) throw err; 
       console.log('Connected to PostgresSQL');
     })
-    function xp(){
+    function xp() {
       let min = 4
       let max = 15
-      return Math.floor(Math.random() * (max - min + 1)) + min;
+      Math.floor(Math.random() * (max - min + 1)) + min;
     };
-    function curlvl(){
+    function curlvl() {
       let min = 0
       let max = 1000
-      return Math.floor(0.1 * Math.sqrt(row.xp + 0.1));
+      Math.floor(0.1 * Math.sqrt(row.xp + 0.1));
     };
     pool.query(`SELECT * FROM XP WHERE userid = '${message.author.id}'`, (err, row) => {
       if(err) throw err;
       let sql;
       if(row.length < 1) {
-          sql = `INSERT INTO XP (userid, xp, level) VALUES ('${message.author.id}', ${xp()}, ${curlvl()})`
+          sql = `INSERT INTO XP (userid, xp, level) VALUES ('${message.author.id}', ${xp()}, ${curlvl})`
       } else {
         sql = `UPDATE XP SET xp = ${xp + curlvl} WHERE userid = '${message.author.id}'`
       }
