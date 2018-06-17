c04d19dc561177bd55afce3e09efcd2232debd5c
Akashic Bearer <35845153+AkashicBearer@users.noreply.github.com>
2018-06-17 22:41:59 +0200

    Update pool.js

--- node-postgres/pool.js
+++ node-postgres/pool.js
@@ -13,14 +13,5 @@ const config = {
 };
 
 var pool = new pg.Pool(config);
-  pool.connect();
-
-pool.query('SELECT * FROM xp', (err, res) => {
-  if (err) throw err;
-  for (let row of res.rows) {
-    console.log(JSON.stringify(row));
-  }
-  pool.end();
-})  
 
 module.exports = pool;
