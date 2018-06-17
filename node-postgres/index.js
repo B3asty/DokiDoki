9c9e87d4b9069412aa7979fd78d56b294ff3da9c
Akashic Bearer <35845153+AkashicBearer@users.noreply.github.com>
2018-06-17 17:47:12 +0200

    Update index.js

--- node-postgres/index.js
+++ node-postgres/index.js
@@ -1,41 +1,16 @@
-const { Pool, Client } = require('pg');
-const connectionString = process.env.DATABASE_URL;
+const { Client } = require('pg');
 
-const pool = new Pool({
-  connectionString: connectionString,
-})
 const client = new Client({
-  connectionString: connectionString,
-})
-	client.on(`/db`, async (req, res) => {
-  		try{
-  			const client1 = await pool.connect()
-  			const result = await client.query(`SELECT * FROM xp`)
-  			res.render('pages/db', result)
-  			client.release()
-  		} catch (err) {
-  			console.error(err);
-  			res.send("Error " + err);
-  		}
-  	})
-
-pool.query('SELECT NOW()', (err, res) => {
-  console.log(err, res)
-  pool.end()
-})
+  connectionString: process.env.DATABASE_URL,
+  ssl: true,
+});
 
 client.connect();
-client.query('SELECT * FROM XP', (err, res) => {
+
+client.query('SELECT table_schema,table_name FROM information_schema.tables;', (err, res) => {
   if (err) throw err;
   for (let row of res.rows) {
     console.log(JSON.stringify(row));
   }
   client.end();
 });
-
-client.connect()
-
-client.query('SELECT NOW()', (err, res) => {
-  console.log(err, res)
-  client.end()
-})
