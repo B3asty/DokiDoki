67a7929f4b92ec941dacb2496e0e1ba37fd2e7af
Akashic Bearer <35845153+AkashicBearer@users.noreply.github.com>
2018-06-17 19:33:17 +0200

    Update pool.js

--- node-postgres/pool.js
+++ node-postgres/pool.js
@@ -1,18 +1,23 @@
 const PG = require('pg');
 
-const pg = new Client({
-  connectionString: process.env.DATABASE_URL,
+const config = {
+  user: process.env.DATABASE_URL,
+  database: process.env.DATABASE_URL,
+  password: process.env.DATABASE_URL,
+  host: process.env.DATABASE_URL,
+  port: 5432,
   ssl: true,
-});
+  idleTimeoutMillis: 50000
+};
 
-pg.connect();
+var pool = new pg.Pool(config);
+  pool.connect();
 
-pg.query('SELECT table_schema,table_name FROM information_schema.tables;', (err, res) => {
+pool.query('SELECT * FROM xp', (err, res) => {
   if (err) throw err;
   for (let row of res.rows) {
     console.log(JSON.stringify(row));
   }
-  client.end();
-})
-
+  pool.end();
+})  
 module.exports = pool;
