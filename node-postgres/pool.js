faba959c5f91e604dfec34bbbc51e68de9b12ec9
Akashic Bearer <35845153+AkashicBearer@users.noreply.github.com>
2018-06-17 22:37:27 +0200

    Update pool.js

--- node-postgres/pool.js
+++ node-postgres/pool.js
@@ -7,6 +7,7 @@ const config = {
   host: "ec2-54-225-96-191.compute-1.amazonaws.com",
   database: "d3u98tvto5gblt",
   user: "mncmdnttojdsku",
+  password: "3849194dc1c60408b302cd9991f3ce27e08827998d5029ae25080e6bf09ee779",
   ssl: true,
   idleTimeoutMillis: 50000
 };
