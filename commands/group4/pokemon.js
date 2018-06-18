43896359e0d5f8e59606ce911f84b7c66ed26ae3
Kandrina <manuela.greifoner@gmx.at>
2018-06-18 20:58:42 +0200

    pokemon

---
+++ commands/group4/pokemon.js
@@ -0,0 +1,31 @@
+const { Command } = require('discord.js-commando');
+const { RichEmbed } = require('discord.js');
+const oakdexPokedex = require('oakdex-pokedex');
+
+module.exports = class pokemonCommand extends Command {
+    constructor(client) {
+      super(client, {
+            name: 'pokemon',
+            aliases: ['pokedex', 'pkmn', 'dex'],
+            group: 'group4',
+            memberName: 'pokemon',
+            description: 'Gives you information about a Pokemon',
+            args: [
+                {
+                    key: 'name',
+                    prompt: 'Please name a Pokemon, or enter an ID',
+                    type: 'string'
+                }
+            ]
+        });
+    }
+
+	async run(msg, args) {
+           const embed = new RichEmbed()
+           var poke = args.name;
+            oakdexPokedex.findPokemon(poke, function(p) {
+              // returns data/pokemon/eevee.json
+              console.log(p); // Eeevee
+            });    
+        }
+	};
