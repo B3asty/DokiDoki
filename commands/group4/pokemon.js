85b6c3e20f0b63d622ee55914318ad119c8b95b9
Kandrina <manuela.greifoner@gmx.at>
2018-06-18 23:35:13 +0200

    p

--- commands/group4/pokemon.js
+++ commands/group4/pokemon.js
@@ -22,7 +22,66 @@ module.exports = class pokemonCommand extends Command {
     }
 
 	async run(msg, args) {
-           const embed = new RichEmbed()
+
+        var poke = args.name.charAt(0).toUpperCase() + args.name.slice(1);
+        oakdexPokedex.findPokemon(poke, function(p) {
+            console.log(p);
+          embed.setTitle('#'+p.national_id+ " "+p.names.en)
+
+           
+          var thum = "https://img.pokemondb.net/sprites/"
+          if(p.pokedex_entries.Sun){
+            embed.setDescription(p.pokedex_entries.Sun.en + "\n\n" + p.pokedex_entries.Moon.en)
+            thum = thum + "/sun-moon/dex/normal/"
+          }else if(p.pokedex_entries.X){
+            embed.setDescription(p.pokedex_entries.X.en + "\n\n" + p.pokedex_entries.Y.en)
+            thum = thum + "/x-y/dex/normal/"
+          }else if(p.pokedex_entries.Black){
+            embed.setDescription(p.pokedex_entries.Black.en + "\n\n" + p.pokedex_entries.White.en)
+            thum = thum + "/black-white/dex/normal/"
+          }else if(p.pokedex_entries.Diamond){
+            embed.setDescription(p.pokedex_entries.Diamond.en + "\n\n" + p.pokedex_entries.Pearl.en + "\n\n" + p.pokedex_entries.Platinum.en)
+            thum = thum + "/diamond-pearl/dex/normal/"
+          }else if(p.pokedex_entries.Ruby){
+            embed.setDescription(p.pokedex_entries.Ruby.en + "\n\n" + p.pokedex_entries.Sapphire.en+ "\n\n" + p.pokedex_entries.Emerald.en)
+            thum = thum + "/ruby-sapphire/dex/normal/"
+          }else if(p.pokedex_entries.Gold){
+            embed.setDescription(p.pokedex_entries.Gold.en + "\n\n" + p.pokedex_entries.Silver.en+ "\n\n" + p.pokedex_entries.Crystal.en)
+            thum = thum + "/silver/dex/normal/"
+          }else if(p.pokedex_entries.Red){
+            embed.setDescription(p.pokedex_entries.Red.en + "\n\n" + p.pokedex_entries.Yellow.en)
+            thum = thum + "/red-blue/dex/normal/"
+          }
+
+
+          thum = thum+p.names.en.toLowerCase()+".png"
+
+        embed.setThumbnail(thum)
+          
+          var names = "";
+
+          if(p.names.jp){
+            names = names + "**JP:** " + p.names.jp + "\n"
+          }
+          if(p.names.de){
+            names = names + "**DE:** " + p.names.de + "\n"
+          }
+          if(p.names.fr){
+            names = names + "**FR:** " + p.names.fr + "\n"
+          }
+          if(p.names.es){
+            names = names + "**ES:** " + p.names.es + "\n"
+          }
+          if(p.names.it){
+            names = names + "**IT:** " + p.names.it + "\n"
+          }
+          
+          embed.addField("Names", names)
+
+          msg.channel.send(embed)
+
+
+          /* const embed = new RichEmbed()
            var poks = args.name.toLowerCase().replace(" ", "-");
 
            pok.getPokemon(args.name)
@@ -102,7 +161,7 @@ module.exports = class pokemonCommand extends Command {
                    
                 });
             console.log(err)
-           })
+           })*/
            
         }
 	};
