73ef7e9af7a86ea88fccbd4b770e038c9053531b
Kandrina <manuela.greifoner@gmx.at>
2018-06-18 21:34:24 +0200

    p

--- commands/group4/pokemon.js
+++ commands/group4/pokemon.js
@@ -23,15 +23,11 @@ module.exports = class pokemonCommand extends Command {
 	async run(msg, args) {
            const embed = new RichEmbed()
            var poke = args.name.charAt(0).toUpperCase() + args.name.slice(1);
-            oakdexPokedex.findPokemon(poke)
-            .then(p => {
-
+            oakdexPokedex.findPokemon(poke, function(p) {
                 console.log(p);
               //embed.setTitle('#'+p.national_id+ " "+names.en)
               embed.setThumbnail()
-            })
-            .throw(err => {
-                console.log(err)
-            })   
+               
+            });    
         }
 	};
