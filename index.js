eb07a7b48cf850083981d72d122480f48d6bcc37
Akashic Bearer <35845153+AkashicBearer@users.noreply.github.com>
2018-06-18 17:07:18 +0200

    Update index.js

--- index.js
+++ index.js
@@ -97,7 +97,7 @@ if (message.author.bot) return;
     if (rows.length < 1){
 		sql = `INSERT INTO xp(userid, xp, level) VALUES('${message.author.id}', 0, 0)`
     } else {
-    	let xp = rows.xp;
+    	let xp = [].xp;
         sql = `UPDATE xp SET xp = ${xp + xpgen} WHERE userid = '${message.author.id}'`
      }
      pool.query(sql, console.log);
