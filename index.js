5c536018dd761509ad0635d741008c21e382363c
Akashic Bearer <35845153+AkashicBearer@users.noreply.github.com>
2018-06-18 17:33:26 +0200

    Update index.js

--- index.js
+++ index.js
@@ -89,16 +89,20 @@ if (message.author.bot) return;
       console.log('Connected to PostgresSQL');
     })
 
-    pool.query(`SELECT * FROM xp WHERE userid = '${message.author.id}'`, {useArray: true}, (err, rows, array) => {
+    pool.query(`SELECT * FROM xp WHERE userid = '${message.author.id}'`, {useArray: true}, (err, rows) => {
+	    
     const curlvl = Math.floor(0.1 * Math.sqrt(rows.xp + 0.1));
     const xpgen = Math.floor(Math.random() * (20 - 5 + 1)) + 5;
+	    
     if(err) throw err;
       	let sql;
+	    
     if (!rows.length < 1){
-		sql = `INSERT INTO xp(userid, xp, level) VALUES('${message.author.id}', 0, 0)`
+	sql = `INSERT INTO xp(userid, xp, level) VALUES('${message.author.id}', 0, 0)`
     } else {
-        sql = `UPDATE xp SET xp = ${xpgen} WHERE userid = ${message.author.id}`
+        sql = `UPDATE xp SET xp = ${xpgen} WHERE userid = '${message.author.id}'`
      };
+	    
      pool.query(sql, console.log);
      pool.end(err => {
       if(err) throw err; 
