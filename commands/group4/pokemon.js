fc1c0ab19038fa0967d7bac9638458d9c9dae1ce
Kandrina <manuela.greifoner@gmx.at>
2018-06-18 21:37:55 +0200

    p

--- commands/group4/pokemon.js
+++ commands/group4/pokemon.js
@@ -25,8 +25,11 @@ module.exports = class pokemonCommand extends Command {
            var poke = args.name.charAt(0).toUpperCase() + args.name.slice(1);
             oakdexPokedex.findPokemon(poke, function(p) {
                 console.log(p);
-              //embed.setTitle('#'+p.national_id+ " "+names.en)
-              embed.setThumbnail()
+              embed.setTitle('#'+p.national_id+ " "+names.en)
+              var sprite = "../../node_modules/oakdex-pokedex-sprites/icons/"+p.national_id+".png";
+              embed.setThumbnail(sprite)
+
+              msg.channel.send(embed)
                
             });    
         }
