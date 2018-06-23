431adabab88326e24abd598ef4e04784fcaa8227
Akashic Bearer <35845153+AkashicBearer@users.noreply.github.com>
2018-06-23 14:26:24 +0200

    Update index.js

--- index.js
+++ index.js
@@ -119,7 +119,7 @@ client.on("message", (message) => {
               host: process.env.dbhost,
               database: process.env.db,
               user: process.env.user,
-            password: process.env.password,
+              password: process.env.password,
               ssl: true,
         });
     
@@ -130,9 +130,7 @@ client.on("message", (message) => {
 
     pool.query(`SELECT xp, level FROM xp WHERE userid = '193021560792154112'`, (err, result) => {
       console.log(result)
-        console.log(result.rows[0].anonymous)
-  console.log(result.rows[0].JSON.parse(anonymous))
-
+        console.log(result.rows["anonymous"].xp)
       pool.end(err => {
         if(err) throw err; 
         console.log('Logged to PostgresSQL');
