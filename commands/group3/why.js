1250703a3a0cf09710f3bd5060d84914c53d9836
Kandrina <manuela.greifoner@gmx.at>
2018-06-27 08:47:15 +0200

    testing

---
+++ commands/group3/why.js
@@ -0,0 +1,27 @@
+const { Command } = require('discord.js-commando');
+const { RichEmbed } = require('discord.js');
+const neko = require("nekos.life");
+const superagent = require("superagent");
+const send = require("quick.hook");
+
+module.exports = class WhyCommand extends Command {
+    constructor(client) {
+        super(client, {
+            name: 'why',
+            aliases: [],
+            group: 'group3',
+            memberName: 'why',
+            description: 'why?',
+        });
+    }
+	async run(msg, args, neko) { 
+	const {body} = await superagent 
+        .get('https://nekos.life/api/v2/why')
+        const embed = new RichEmbed()
+	        embed.setTitle('Random Fact~')
+            console.log(body.why)
+            embed.setDescription(`${body.why}`)
+            embed.setColor('RANDOM')
+        return msg.embed(embed);
+	}
+};
